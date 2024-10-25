685b00fc18a51e959acf54e6204f6d01f7a33fd7
hamza46awais <121958714+hamza46awais@users.noreply.github.com>
2024-10-25 11:30:38 +0100

    Create library.sql

---
+++ library.sql
@@ -0,0 +1,103 @@
+CREATE DATABASE library;
+
+CREATE TABLE book (
+	id INT PRIMARY KEY AUTO_INCREMENT, 
+	name VARCHAR(50) NOT NULL, 
+    author_id VARCHAR(50) NOT NULL,
+    category VARCHAR(120) NOT NULL,
+    publisher_id VARCHAR(50) NOT NULL,
+    description VARCHAR(120) NOT NULL,
+    edition INT,
+    language VARCHAR(50),
+    release_year INT
+); 
+
+CREATE TABLE author (
+	id INT PRIMARY KEY AUTO_INCREMENT, 
+	first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    author_id VARCHAR(50) NOT NULL,
+    is_active BOOLEAN
+); 
+
+CREATE TABLE publisher (
+	id INT PRIMARY KEY AUTO_INCREMENT, 
+	name VARCHAR(50) NOT NULL, 
+    author_id VARCHAR(50) NOT NULL
+); 
+
+CREATE TABLE category (
+	id INT PRIMARY KEY AUTO_INCREMENT, 
+	name VARCHAR(50) NOT NULL
+); 
+
+CREATE TABLE section (
+	id INT PRIMARY KEY AUTO_INCREMENT, 
+	name VARCHAR(50) NOT NULL
+); 
+
+CREATE TABLE inventory (
+	id INT PRIMARY KEY AUTO_INCREMENT, 
+    floor INT,
+    number INT
+); 
+
+CREATE TABLE person (
+	id INT PRIMARY KEY AUTO_INCREMENT, 
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+	name VARCHAR(50) NOT NULL
+); 
+
+CREATE TABLE rental (
+	id INT PRIMARY KEY AUTO_INCREMENT, 
+	name VARCHAR(50) NOT NULL
+); 
+
+CREATE TABLE staff (
+	id INT PRIMARY KEY AUTO_INCREMENT, 
+    position VARCHAR(50),
+    years_served INT,
+	name VARCHAR(50) NOT NULL
+); 
+
+CREATE TABLE customer (
+	id INT PRIMARY KEY AUTO_INCREMENT, 
+	name VARCHAR(50) NOT NULL
+); 
+
+CREATE TABLE payment (
+	id INT PRIMARY KEY AUTO_INCREMENT, 
+	name VARCHAR(50) NOT NULL
+); 
+
+CREATE TABLE book_author (
+	id INT PRIMARY KEY AUTO_INCREMENT, 
+	name VARCHAR(50) NOT NULL
+); 
+
+CREATE TABLE book_category (
+	id INT PRIMARY KEY AUTO_INCREMENT, 
+	name VARCHAR(50) NOT NULL
+); 
+
+CREATE TABLE rental (
+	id INT PRIMARY KEY AUTO_INCREMENT, 
+    rental_date DATE, 
+    rental_duration INT,
+    return_date DATE,
+    book_id INT,
+    customer_id INT,
+	name VARCHAR(50) NOT NULL
+); 
+
+CREATE TABLE events (
+	id INT PRIMARY KEY AUTO_INCREMENT, 
+	name VARCHAR(50) NOT NULL,
+	start_date DATE,
+    end_date DATE, 
+    start_time TIME,
+    end_time TIME,
+    event_type VARCHAR(50),
+    recurring BOOLEAN
+); 
